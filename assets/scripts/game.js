026128a7d3f171f9ed0479a88d88e73600d7d435
Jonathan C <jsea@uw.edu>
2019-09-06 16:26:49 -0700

    Set up the initial game object and wrote out high-level pseudo code to lead the way.

---
+++ assets/scripts/game.js
@@ -0,0 +1,25 @@
+
+// Create an object to contain all of our game logic
+let game = {
+
+    // Set up necessary variables to keep track of the game state
+
+    // Initialize the Game (happens on page load)
+
+    // Set up a new game
+
+    // Load a new question to ask along with its answers
+
+    // Handle a Correct Guess
+
+    // Handle an Incorrect Guess
+
+    // Handle the Time Running Out
+
+    // Handle game completion
+
+    // Set up the different State Objects to switch between Game States
+
+    // Set up the method to handle switching between Game States
+
+};
